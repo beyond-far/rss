bbbc64b43d04825affc8d5461b9abc2effab3c45
陈明 <chenming@ergengtv.com>
2017-05-22 16:28:27 +0800

    modify

--- diy/configs.py
+++ diy/configs.py
@@ -2,5 +2,5 @@
 
 WEIBO_URL = 'http://service.weibo.com/widget/widget_blog.php?uid={uid}'
 WEIBO_LINK = 'http://weibo.com/u/{uid}'
-NEW_WEIBO_URL = 'https://m.weibo.cn/api/container/getIndex?type=uid&value={uid}&containerid=107603{uid}'
-
+NEW_WEIBO_URL = 'http://m.weibo.cn/u/{uid}'
+NEW_WEIBO_API = 'https://m.weibo.cn/api/container/getIndex?type=uid&value={uid}&containerid=107603{uid}'
--- diy/handlers/weibo.py
+++ diy/handlers/weibo.py
@@ -6,7 +6,7 @@ import tornado.gen
 import json
 from pyquery import PyQuery as pq
 from utils.filters import weibodate, id2mid
-from configs import WEIBO_URL, WEIBO_LINK, NEW_WEIBO_URL
+from configs import WEIBO_URL, WEIBO_LINK, NEW_WEIBO_URL, NEW_WEIBO_API
 
 
 class WeiboHandler(tornado.web.RequestHandler):
@@ -16,13 +16,13 @@ class WeiboHandler(tornado.web.RequestHandler):
         if uid and uid.isdigit() and len(uid) == 10:
             self.uid = uid
             client = tornado.httpclient.AsyncHTTPClient()
-            response = yield client.fetch(NEW_WEIBO_URL.format(uid=uid))
+            response = yield client.fetch(NEW_WEIBO_API.format(uid=uid))
 
             if response.error:
                 raise tornado.web.HTTPError(response.code)
             else:
                 cards = json.loads(response.body)['cards']
-                title = u'{author}的微博'.format(author=cards[0]['mblog']['user']['screen_name'])
+                retitle = u'{author}的微博'.format(author=cards[0]['mblog']['user']['screen_name'])
                 items = []
                 for card in cards:
                     item = {}
@@ -54,8 +54,8 @@ class WeiboHandler(tornado.web.RequestHandler):
                     item['author'] = card['mblog']['user']['screen_name']
                     items.append(item)
                 pubdate = items[0]['created']
-                link = WEIBO_URL.format(uid=uid)
+                link = NEW_WEIBO_URL.format(uid=uid)
                 self.set_header("Content-Type", "application/xml")
-                self.render("rss.xml", title=title, description=title, items=items, pubdate=pubdate, link=link)
+                self.render("rss.xml", title=retitle, description=retitle, items=items, pubdate=pubdate, link=link)
         else:
             self.redirect("/")
